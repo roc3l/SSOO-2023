0e8e00a0a5b919fc6ade8813f7865d16c6a7999e
roc3l <136416918+roc3l@users.noreply.github.com>
2023-09-07 10:56:23 -0300

    ej3

--- labs/Ejercicio1.c
+++ labs/Ejercicio1.c
@@ -2,11 +2,11 @@
 int main() {
     int a=0;
     int sum=0;
+    
     printf("sumando los valores de:");
-    for (a=1;a<=100;a=a+2){
+    for (a=1;a<=200;a=a+2){
         printf(" %d",a);
         sum=sum+a;
-
     }
     printf("\nel resultado final es %d",sum);
     return 0;
---
+++ labs/Ejercicio4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+    int l1,l2,l3;
+    scanf("%d",&l1);
+    scanf("%d",&l2);
+    scanf("%d",&l3);
+
+    if(l1==l2){
+      if(l2==l3){
+        printf("es un triangulo equilatero");
+      }
+      if(l2!=l3){
+        printf("es un triangulo isosceles");
+      }
+    }
+
+    else if(l1==l3){
+      if(l2==l3){
+        printf("es un triangulo equilatero");
+      }
+      if(l2!=l3){
+        printf("es un triangulo isosceles");
+      }
+    }
+  
+    else if(l2==l3){
+      if(l2==l1){
+        printf("es un triangulo equilatero");
+      }
+      if(l2!=l1){
+        printf("es un triangulo isosceles");
+      }
+    }
+  
+    else if(l1!=l2&&l1!=l3&&l2!=l3){
+      printf("es un triangulo escaleno");
+    }
+
+    int perimetro,semiperimetro,areaheron;
+
+    return 0;
+}
